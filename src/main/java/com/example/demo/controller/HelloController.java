4214e791ee0ab37bf6dcfbf5a870b9cc59eb50ad
leon <leon@DESKTOP-5581TI1>
2017-11-19 00:22:30 +0800

    增加docker file

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,6 +1,5 @@
 package com.example.demo.controller;
 
-import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
