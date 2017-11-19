2144a962afff2be73356f2e061a8376d5d8dc352
liwei <1137110730@qq.com>
2017-11-19 12:12:24 +0800

    增加服务器信息

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,6 +1,10 @@
 package com.example.demo.controller;
 
 
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,8 +17,16 @@ public class HelloController {
 	public String hello()
 	{
 		SupplierManager.printSupplierMap();
+		String serverIp = null;
 		//return "hello";
-		return SupplierManager.getSupplier("xiaoka").query("hello");
+		try {
+			serverIp =  InetAddress.getLocalHost().getHostAddress();
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}//获得本机IP  
+		
+		return SupplierManager.getSupplier("xiaoka").query(serverIp);
 	}
 	
 }
